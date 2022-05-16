7798238f37ef240206b60ab738e6f91fd4854b67
Dydson <familiasouza999@gmail.com>
2022-05-16 20:44:06 -0300

    receba

--- imc.php
+++ imc.php
@@ -22,21 +22,21 @@ $total = $peso/($altura**2);
 
 
  if($total < 16){
-    echo"Destnutrição grau 3";
+    echo"Voçê está com Destnutrição grau 3";
 } elseif($total >= 16 && $total <17){
-    echo"Destnutrição grau 2";
+    echo"Voçê está com Destnutrição grau 2";
 } elseif($total >= 17 && $total <18.5){
-    echo"Destnutrição grau 1";
+    echo"Voçê está com Destnutrição grau 1";
 } elseif($total >= 18.5 && $total <24.9){
-    echo"Normal";
+    echo"Voçê está com Normal";
 } elseif($total >= 25 && $total <30){
-    echo"Sobrepeso";
+    echo"Voçê está com Sobrepeso";
 } elseif($total >= 30 && $total <35){
-    echo"Obesidade Grau 1 ";
+    echo"Voçê está com Obesidade Grau 1 ";
 } elseif($total >= 35 && $total <40){
-    echo"Obesidade Grau 2 ";
+    echo"Voçê está com Obesidade Grau 2 ";
 } elseif($total > 40){
-    echo"Obesidade Grau 3";
+    echo"Voçê está com Obesidade Grau 3";
 }
 
 
